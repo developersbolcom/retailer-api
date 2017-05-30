518c6c4aa62e164db3553ca33022a24a3356f3cb
Andreea Marin <amarin@bol.com>
2017-05-30 15:25:11 +0200

    Update on the website at Tue May 30 15:25:11 CEST 2017

--- static/assets/js/index.js
+++ static/assets/js/index.js
@@ -1,36 +1,56 @@
-/*
-===============================================================================
-  Subbscribe
-===============================================================================
-*/
-if (window.subbscribeOpts) {
-  $('body').subbscribe(window.subbscribeOpts);
-}
-/*
-===============================================================================
-  Infinity Scroll - YEAH!
-===============================================================================
-*/
-    $('.index-wrapper').infinitescroll({
-        navSelector: "#next:last",
-        nextSelector: "a#next:last",
-        itemSelector: "#timeline",
-        dataType: 'html',
-        maxPage: 3,
+/**
+ * Main JS file for Casper behaviours
+ */
+
+/* globals jQuery, document */
+(function ($, undefined) {
+    "use strict";
+
+    var $document = $(document);
+
+    $document.ready(function () {
+
+        var $postContent = $(".post-content");
+        $postContent.fitVids();
+
+        $(".scroll-down").arctic_scroll();
+
+        $(".menu-button, .nav-cover, .nav-close").on("click", function(e){
+            e.preventDefault();
+            $("body").toggleClass("nav-opened nav-closed");
+        });
+
     });
-/*
-===============================================================================
-  Disqus
-===============================================================================
-*/
-if (window.disqusSrc) {
-  (function () { // DON'T EDIT BELOW THIS LINE
-      var d = document,
-          s = d.createElement('script');
 
-      s.src = window.disqusSrc;
+    // Arctic Scroll by Paul Adam Davis
+    // https://github.com/PaulAdamDavis/Arctic-Scroll
+    $.fn.arctic_scroll = function (options) {
 
-      s.setAttribute('data-timestamp', +new Date());
-      (d.head || d.body).appendChild(s);
-  })();
-}
+        var defaults = {
+            elem: $(this),
+            speed: 500
+        },
+
+        allOptions = $.extend(defaults, options);
+
+        allOptions.elem.click(function (event) {
+            event.preventDefault();
+            var $this = $(this),
+                $htmlBody = $('html, body'),
+                offset = ($this.attr('data-offset')) ? $this.attr('data-offset') : false,
+                position = ($this.attr('data-position')) ? $this.attr('data-position') : false,
+                toMove;
+
+            if (offset) {
+                toMove = parseInt(offset);
+                $htmlBody.stop(true, false).animate({scrollTop: ($(this.hash).offset().top + toMove) }, allOptions.speed);
+            } else if (position) {
+                toMove = parseInt(position);
+                $htmlBody.stop(true, false).animate({scrollTop: toMove }, allOptions.speed);
+            } else {
+                $htmlBody.stop(true, false).animate({scrollTop: ($(this.hash).offset().top) }, allOptions.speed);
+            }
+        });
+
+    };
+})(jQuery);
